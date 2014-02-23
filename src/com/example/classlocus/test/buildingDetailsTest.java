306d5f33c8f8b733564e7c7ea53e7d14d6a51a15
Anthony <anthonymiller@outlook.com>
2014-02-22 17:51:05 -0800

    Add intent for building object

--- src/com/example/classlocus/test/buildingDetailsTest.java
+++ src/com/example/classlocus/test/buildingDetailsTest.java
@@ -1,22 +1,25 @@
 package com.example.classlocus.test;
+import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
 import android.widget.TextView;
 
 import com.example.classlocus.BuildingDetail;
 import com.example.classlocus.R;
-import com.google.android.gms.maps.model.LatLng;
 
-public class buildingDetailsTest extends ActivityInstrumentationTestCase2<BuildingDetail> {
+public class BuildingDetailsTest extends ActivityInstrumentationTestCase2<BuildingDetail> {
 
 	BuildingDetail activity;
 	
-	public buildingDetailsTest() {
+	public BuildingDetailsTest() {
 		super(BuildingDetail.class);
 	}
 
 	protected void setUp() throws Exception {
 		super.setUp();
+		Intent i = new Intent(getInstrumentation().getTargetContext(),
+				BuildingDetail.class);
 		activity = getActivity();
+		
 	}
 
 	protected void tearDown() throws Exception {
@@ -39,24 +42,5 @@ public class buildingDetailsTest extends ActivityInstrumentationTestCase2<Buildi
 		assertEquals(bName.getText(), "KEC");
 		assertEquals(bDetails.getText(), "KEC");
 	}
-	
-	public final void testDistanceClose(){
-		//Distance from the intersection near weatherford to that same location
-		String testString = activity.buildingDistance(new LatLng(Double.parseDouble("44.564591"), 
-				Double.parseDouble("-123.279995")), 
-				new LatLng(Double.parseDouble("44.564591"), 
-						Double.parseDouble("-123.279995")));
-		assertEquals(testString, testString, "0.0m");
-	}
 
-	public final void testDistanceFar(){
-		//distance between the intersection near weatherford and the intersection near Kerr
-		//1.5hrs and counting! starting again at 1:17PM
-		String testString = activity.buildingDistance(
-				new LatLng(Double.parseDouble("44.564591"), Double.parseDouble("-123.279995")),
-				new LatLng(Double.parseDouble("44.564576"), Double.parseDouble("-123.274029")));
-		testString = testString.substring(0, testString.indexOf('.'));
-		assertEquals(testString, String.valueOf(testString.charAt(0)), "4");
-	}
-	
 }
