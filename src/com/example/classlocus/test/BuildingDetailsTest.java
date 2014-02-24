655b329d780e8a8d9ba198965e10d0f843240a9c
Anthony <anthonymiller@outlook.com>
2014-02-23 17:18:39 -0800

    Expand building test and create building object

--- src/com/example/classlocus/test/BuildingDetailsTest.java
+++ src/com/example/classlocus/test/BuildingDetailsTest.java
@@ -1,8 +1,13 @@
 package com.example.classlocus.test;
+import android.content.Context;
+import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.mock.MockContext;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.example.classlocus.BuildingDetail;
+import com.example.classlocus.data.*;
 import com.example.classlocus.R;
 
 public class BuildingDetailsTest extends ActivityInstrumentationTestCase2<BuildingDetail> {
@@ -28,15 +33,21 @@ public class BuildingDetailsTest extends ActivityInstrumentationTestCase2<Buildi
 	}
 	
 	public final void testBuildingDetails() {
-		fail("There is not any procedure to input data into the page yet");
+		BuildingsDataSource bds = new BuildingsDataSource(activity);
+		assertNotNull(bds);
+		Building kelley = bds.createBuilding("Kelley Engineering Center", "KEC"); //Null pointer exception here!
+		//Intent i = new Intent(activity, BuildingDetail.class);
+		//i.putExtra("buildingID", 7);
+		//activity.populate(i);
 		//put data into the views.
-		TextView bName = (TextView) activity.findViewById(R.id.detail_building_name);
+		/*TextView bName = (TextView) activity.findViewById(R.id.detail_building_name);
 		TextView bDetails = (TextView) activity.findViewById(R.id.detail_building_value);
 		assertNotNull(bName);
 		assertNotNull(bDetails);
 		//TODO Fill these to match dummy data.
 		assertEquals(bName.getText(), "KEC");
-		assertEquals(bDetails.getText(), "KEC");
+		assertEquals(bDetails.getText(), "KEC");*/
+		bds.deleteBuilding(kelley);
 	}
 
 }
