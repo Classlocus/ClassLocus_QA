af6cede17d93fd55d675fa5c4a3e0c7bbaadb5af
Anthony <anthonymiller@outlook.com>
2014-03-08 15:16:52 -0800

    Adding building details populate test

--- src/com/example/classlocus/test/BuildingDetailsTest.java
+++ src/com/example/classlocus/test/BuildingDetailsTest.java
@@ -1,13 +1,22 @@
 package com.example.classlocus.test;
+import java.util.ArrayList;
+import java.util.List;
+
 import android.test.ActivityInstrumentationTestCase2;
 import android.widget.TextView;
+import android.content.Intent;
 
 import com.example.classlocus.BuildingDetail;
 import com.example.classlocus.R;
+import com.example.classlocus.data.Building;
+import com.example.classlocus.data.BuildingsRepository;
+import com.example.classlocus.data.DatabaseHelper;
 
 public class BuildingDetailsTest extends ActivityInstrumentationTestCase2<BuildingDetail> {
 
 	BuildingDetail activity;
+	private BuildingsRepository repository;
+	private Building building;
 	
 	public BuildingDetailsTest() {
 		super(BuildingDetail.class);
@@ -37,6 +46,25 @@ public class BuildingDetailsTest extends ActivityInstrumentationTestCase2<Buildi
 		//TODO Fill these to match dummy data.
 		assertEquals(bName.getText(), "KEC");
 		assertEquals(bDetails.getText(), "KEC");
+	}
+	
+	public void testBuildingPopulate() {
+		repository = new BuildingsRepository(activity);
+		building = new Building();
+		Intent search = new Intent();
+		long bid;
+		List<Building> bdlist = new ArrayList<Building>();
+		
+		building.setName("Kelley Center");
+		building.setAbbreviation("KEC");
+		building.setLatLng(23.45, 34.56);
+		
+		repository.saveBuilding(building);
+		bdlist.add(repository.searchBuilding("Kelley").get(0));
+		bid = bdlist.get(0).getId();
+		search.putExtra("buildingID", bid);
+		bdlist.add(activity.populate(search, repository));
+		
 	}
 
 }
